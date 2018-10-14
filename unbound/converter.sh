9bee8345ccc33075ca8f0e57affc18b47440dec4
archfate <archfae@protonmail.com>
2018-10-13 21:02:28 -1000

    added unbound

---
+++ unbound/converter.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/sh
+
+file=adservers
+
+wget -N https://raw.githubusercontent.com/missdeer/blocklist/master/toblock-without-shorturl-optimized.lst
+echo "# Converted from https://github.com/missdeer/blocklist/blob/master/toblock-without-shorturl-optimized.lst" > $file
+echo "# https://github.com/missdeer/blocklist" >> $file
+echo "# Thanks to all contributors." >> $file
+echo '' >> $file
+
+cat toblock-without-shorturl-optimized.lst | awk '{print "0.0.0.0 " $1}' |  awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' >> $file
+ 
+# Cleanup...
+rm toblock-without-shorturl-optimized.lst
+
+git add .
+git commit -m "`date`"
+git push origin master
